18801566bec31345919a095b99b7cc55988cefd3
Brian <b.perez3237@gmail.com>
2022-07-12 19:24:24 -0400

    fetch all posts in homepage

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,6 +1,14 @@
 import React from "react";
+import {useEffect, useState } from 'react'
 
 function PostList() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=> {
+        fetch('/posts')
+            .then((r) => r.json())
+            .then((data)=>setPosts(data))
+    })
 
     return (
         <>
