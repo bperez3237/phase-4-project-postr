4fd8304e77e43718b72672f076b321f669264763
Brian <b.perez3237@gmail.com>
2022-07-12 18:56:19 -0400

    create react component for posts page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import './App.css';
+import PostList from './components/PostList';
 
 
 function App() {
@@ -18,8 +19,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
+          <Route path="/homepage">
+            <PostList></PostList>
           </Route>
           <Route exact path="/count">
             <h1>Page Count: {count}</h1>
---
+++ client/src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PostList() {
+
+    return (
+        <></>
+    )
+
+
+}
+
+export default PostList;
