5c3b52710662b6056af394d203727d3944909503
Brian <b.perez3237@gmail.com>
2022-07-19 19:56:51 -0400

    Fix post bar

--- client/src/components/NewPost.js
+++ client/src/components/NewPost.js
@@ -7,7 +7,7 @@ function NewPost() {
         <Form className='m-3' style={{borderStyle:'solid'}}>
             <Form.Group className='m-3'>
                 <Form.Label>post here:</Form.Label>
-                <Form.Control></Form.Control>
+                <Form.Control type='text' size='lg'></Form.Control>
             </Form.Group>
             <Button variant='dark' type='submit'>POST!</Button>
         </Form>
