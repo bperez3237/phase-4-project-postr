0b0b3f574be8e48ece393e1a148bbcafa2b8a64a
Brian <b.perez3237@gmail.com>
2022-07-20 17:51:46 -0400

    Change sorting on post list

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -4,7 +4,10 @@ import Post from './Post'
 
 function PostList({location, username: name}) {
 
-    const postElements = location.posts.map((post)=><Post key={post.id} post={post} location={location.name} name={name}></Post>)
+    const postElements = location.posts.sort((a,b)=> Date.parse(b.created_at) - Date.parse(a.created_at)).map((post)=>{
+        // console.log(Date.parse(post.created_at))
+        return <Post key={post.id} post={post} location={location.name} name={name}></Post>}
+        )
 
     return (
         <Container className="m-3" style={{borderStyle:'solid', maxHeight:'900px', overflow: 'scroll'}}>
