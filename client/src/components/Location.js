3c54e390cd93fbcfa9a543511401779446120e51
Brian <b.perez3237@gmail.com>
2022-07-20 20:38:50 -0400

    Red border for selected location on explore page

--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {Card} from 'react-bootstrap'
 
-function Location({location, setLocation}) {
-
+function Location({location, setLocation, selected, setSelectedId}) {
     function handleClick() {
         setLocation(location)
+        setSelectedId(location.id)
+    }
+
+    const style = {
+        borderStyle: 'solid',
+        borderColor: selected ? 'red' : 'black'
     }
 
     return (
-        <Card className="m-4" onClick={handleClick}>
+        <Card className="m-4" onClick={handleClick} style={style}>
             <Card.Header>{location.name}</Card.Header>
             <Card.Body>
                 <Card.Text>
--- client/src/components/LocationList.js
+++ client/src/components/LocationList.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import {Card, Container} from 'react-bootstrap'
 import Location from './Location'
 
 function LocationList({locations, setLocation}) {
+    const [selectedId, setSelectedId] = useState('')
 
-    const locationsElements = locations.map((location)=><Location key={location.id} location={location} setLocation={setLocation}></Location>)
+    const locationsElements = locations.map((location)=>{
+        return <Location 
+        key={location.id} 
+        location={location} 
+        setLocation={setLocation} 
+        selected={location.id==selectedId ? true : false} 
+        setSelectedId={setSelectedId}/>
+    })
 
     return (
        <Container className='h-100' style={{borderStyle:'solid', maxHeight:'900px', overflow: 'scroll'}}>
