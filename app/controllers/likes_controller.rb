c75ea7d2f8712641f80e2e5939354ebb4a20235a
Brian <b.perez3237@gmail.com>
2022-07-24 21:13:41 -0400

    write routes for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,13 @@
 class LikesController < ApplicationController
 
     def create
-
+        like = Like.create(user_id: params[:user_id, post_id: params[:post_id]])
+        render json: like
     end
 
     def destroy
-
+        like = Like.find(params[:id])
+        like.destroy
+        head :no_content
     end
 end
