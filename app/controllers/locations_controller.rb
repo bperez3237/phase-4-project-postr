e1db4d63eaebe0a456d59b29aa8dde63a3c47c80
Brian <b.perez3237@gmail.com>
2022-07-29 16:58:32 -0400

    Re-add locations show route

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,5 +6,9 @@ class LocationsController < ApplicationController
         render json: locations
     end
     
+    def show
+        location = Location.find(params[:id])
+        render json: location
+    end
     
 end
