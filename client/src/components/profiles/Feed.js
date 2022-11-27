73d36e13412f8cccc4451ddc3fd73d4ce83c66fe
Brian <b.perez3237@gmail.com>
2022-11-27 15:30:06 -0500

    Fix profile route bug

--- client/src/components/profiles/Feed.js
+++ client/src/components/profiles/Feed.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import {Route, Switch,  useRouteMatch} from 'react-router-dom'
+import {Route, Switch,  useRouteMatch, useParams} from 'react-router-dom'
 import FeedHeading from './FeedHeading'
 import PostList from '../post/PostList'
 import useGet from '../../hooks/useGet'
 
 function Feed({login}) {
-  let { path, url } = useRouteMatch()
+  let { url } = useRouteMatch()
+  let {username} = useParams()
+
   const [pathState, setPathState] = React.useState('posts')
 
-  const {data: posts, setData: setPosts} = useGet(`/users/${login.user.username}/${pathState}`)
+  const {data: posts, setData: setPosts} = useGet(`/users/${username}/${pathState}`)
 
   return (
     <div className='profile-feeds'>
       <FeedHeading url={url} setPathState={setPathState} />
       <Switch>
-        <Route exact path={path}>
+        <Route exact path={url}>
           <PostList login={login} editable={true} posts={posts} setPosts={setPosts} /> 
         </Route>
         <Route path={`${url}/with_replies`}><h1>Replies</h1></Route>
