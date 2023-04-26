63c4d8b439921bcd29609f88504280b1787e526e
Brian <b.perez3237@gmail.com>
2023-04-26 17:20:56 -0400

    Add location label to login and signup

--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -1,71 +1,86 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import ErrorMessage from "../errors/ErrorMessage";
 
-function LoginForm({loginState, setLoginState, handleChange, optionElements, allLocations, selectedLocation}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState(null);
+function LoginForm({
+  loginState,
+  setLoginState,
+  handleChange,
+  optionElements,
+  allLocations,
+  selectedLocation,
+}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState(null);
 
-   
-   
-    function handleSubmit(e) {
-        e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        const loginObj = {
-            username: username,
-            password: password,
-            location_id: selectedLocation.id
-        }
+    const loginObj = {
+      username: username,
+      password: password,
+      location_id: selectedLocation.id,
+    };
 
-        fetch("/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(loginObj),
-            }).then((r) => {
-                if (r.ok) {
-                    r.json().then((login) => {
-                        setLoginState(login)
-                    });
-                } else {
-                    r.json().then((err) => setErrors(err.errors));
-                }
-            });
-    }
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginObj),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((login) => {
+          setLoginState(login);
+        });
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
 
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      {errors ? <ErrorMessage errors={errors} setErrors={setErrors} /> : null}
+      <div className="mb-4">
+        <label>Username</label>
+        <input
+          type="username"
+          id="username"
+          autoComplete="off"
+          value={username}
+          placeholder="Enter Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label>Password</label>
+        <input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <br />
 
-    return (
-        <form  className="login-form" onSubmit={handleSubmit}>
-            {errors ? <ErrorMessage errors={errors} setErrors={setErrors}/> : null}
-            <div className="mb-4">
-                <label>Username</label>
-                    <input
-                        type="username"
-                        id="username"
-                        autoComplete="off"
-                        value={username}
-                        placeholder="Enter Username"
-                        onChange={(e) => setUsername(e.target.value)}/>
-            </div>
-            <div className="mb-4">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={password}
-                        placeholder="Enter Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-            </div>
-            <br />
-                <select value={loginState.location?.id} onChange={handleChange}>
-                    {allLocations ? optionElements : <></>}
-                </select>
-            <button style={{width:'100%', fontWeight:'600px'}} className='round-button' type='submit'>Login</button>
-        </form>
-    );
+      <div className="mb-4">
+        <label>Location</label>
+        <select value={loginState.location?.id} onChange={handleChange}>
+          {allLocations ? optionElements : <></>}
+        </select>
+      </div>
+      <button
+        style={{ width: "100%", fontWeight: "600px" }}
+        className="round-button"
+        type="submit"
+      >
+        Login
+      </button>
+    </form>
+  );
 }
 
 export default LoginForm;
--- client/src/components/login/SignUpForm.js
+++ client/src/components/login/SignUpForm.js
@@ -1,104 +1,114 @@
-import React, {useState} from "react";
-import {storage} from '../../firebase'
-import {ref, getDownloadURL} from 'firebase/storage'
+import React, { useState } from "react";
+import { storage } from "../../firebase";
+import { ref, getDownloadURL } from "firebase/storage";
 import ErrorMessage from "../errors/ErrorMessage";
 
-function SignUpForm({setLoginState, loginState, handleChange, optionElements, allLocations, selectedLocation}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirmation, setPasswordConfirmation] = useState("");
-    const [name,setName] = useState("")
-    const [errors, setErrors] = useState(null);
-    const [avatarUrl, setAvatarUrl] = useState('')
+function SignUpForm({
+  setLoginState,
+  loginState,
+  handleChange,
+  optionElements,
+  allLocations,
+  selectedLocation,
+}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [name, setName] = useState("");
+  const [errors, setErrors] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState("");
 
+  getDownloadURL(
+    ref(storage, `images/default_image/twitter-avi-gender-balanced-figure.png`)
+  )
+    .then((url) => setAvatarUrl(url))
+    .catch((error) => console.log(error));
 
-    getDownloadURL(ref(storage, `images/default_image/twitter-avi-gender-balanced-figure.png`))
-    .then((url)=>setAvatarUrl(url))
-    .catch((error)=>console.log(error))
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors(null);
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation,
+        name: name,
+        avatar: avatarUrl,
+        location_id: selectedLocation.id,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((login) => {
+          setLoginState(login);
+        });
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
 
-
-  
-    function handleSubmit(e) {
-      e.preventDefault();
-      setErrors(null);
-      fetch("/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          password,
-          password_confirmation: passwordConfirmation,
-          name: name,
-          avatar: avatarUrl,
-          location_id: selectedLocation.id
-        }),
-      }).then((r) => {
-        if (r.ok) {
-          r.json().then((login) => {
-            setLoginState(login)
-          });
-        } else {
-          r.json().then((err) => setErrors(err.errors));
-        }
-      });
-    }
-
-    return (
-        <form className='login-form' onSubmit={handleSubmit}>
-        {errors ? <ErrorMessage errors={errors} setErrors={setErrors}/> : null}
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            id="username"
-            autoComplete="off"
-            value={username}
-            placeholder="Enter Username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            placeholder="Enter Password"
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
-          />
-        </div>
-        <div>
-          <label>Password Confirmation</label>
-          <input
-            type="password"
-            id="password_confirmation"
-            value={passwordConfirmation}
-            placeholder="Confirm Password"
-            onChange={(e) => setPasswordConfirmation(e.target.value)}
-            autoComplete="current-password"
-          />
-        </div>
-        <div>
-          <label>Name</label>
-          <input
-            type="name"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <br />
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      {errors ? <ErrorMessage errors={errors} setErrors={setErrors} /> : null}
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          placeholder="Enter Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+      </div>
+      <div>
+        <label>Password Confirmation</label>
+        <input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          placeholder="Confirm Password"
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+        />
+      </div>
+      <div>
+        <label>Name</label>
+        <input
+          type="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <br />
+      <div className="mb-4">
+        <label>Location</label>
         <select value={loginState.location?.id} onChange={handleChange}>
           {allLocations ? optionElements : <></>}
         </select>
-        <button style={{width:'100%'}} className='round-button' type='submit'>Signup</button>
-      </form>
-    )
+      </div>
 
-
+      <button style={{ width: "100%" }} className="round-button" type="submit">
+        Signup
+      </button>
+    </form>
+  );
 }
 
 export default SignUpForm;
