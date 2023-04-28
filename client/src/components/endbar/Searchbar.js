254cd0f7e36c0564898391eea6137205e793d237
Brian <b.perez3237@gmail.com>
2023-04-28 16:55:24 -0400

    Modify endbar elements

--- client/src/components/endbar/Searchbar.js
+++ client/src/components/endbar/Searchbar.js
@@ -1,19 +1,27 @@
-import React, {useState} from 'react'
-import {BiSearch} from 'react-icons/bi'
+import React, { useState } from "react";
+import { BiSearch } from "react-icons/bi";
 
 function Searchbar() {
-  const [search, setSearch] = useState('')
-  
+  const [search, setSearch] = useState("");
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      window.open(`https://twitter.com/search?q=${search}&src=typed_query`);
+    }
+  };
+
   return (
-    <div className='Searchbar'>
+    <div className="Searchbar">
       <BiSearch />
-      <input 
-        type='text' 
-        placeholder='Search Twitter' 
-        value={search} 
-        onChange={(e)=>setSearch(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Search Twitter"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
     </div>
-  )
+  );
 }
 
-export default Searchbar
+export default Searchbar;
