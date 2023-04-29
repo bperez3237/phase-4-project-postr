020c6803fdc4285f3579942724f84b53cf8fffea
Brian <b.perez3237@gmail.com>
2023-04-29 13:12:34 -0400

    Change settings functions to async await"

--- client/src/components/account/Account.js
+++ client/src/components/account/Account.js
@@ -1,48 +1,101 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-function Account({title, setTitle, file, setFile, uploadImage, uploadBanner}) {
-  const [toggleAvatar, setToggleAvatar] = useState(false)
-  const [toggleBanner, setToggleBanner] = useState(false)
+function Account({
+  title,
+  setTitle,
+  file,
+  setFile,
+  uploadImage,
+  uploadBanner,
+}) {
+  const [toggleAvatar, setToggleAvatar] = useState(false);
+  const [toggleBanner, setToggleBanner] = useState(false);
 
-  useEffect(()=>{
+  const buttonStyle = {
+    backgroundColor: "#274d40",
+    width: "200px",
+  };
+
+  useEffect(() => {
     if (toggleAvatar) {
-      setToggleBanner(false)
+      setToggleBanner(false);
     }
-    
+
     if (toggleBanner) {
-      setToggleAvatar(false)
+      setToggleAvatar(false);
     }
-  }, [toggleAvatar, toggleBanner])
+  }, [toggleAvatar, toggleBanner]);
 
   return (
-    <div className='your-account'>
-        <div className='heading'>
-            <h3>Your Account</h3>
-            <p>See information about your account, download an archive of your data, or learn about your account deactivation options</p>
-        </div>
-        <div className='account-options'>
-            <button className='option' onClick={()=>setToggleAvatar(!toggleAvatar)}>Change profile Picture</button>
-            <button className='option' onClick={()=>setToggleBanner(!toggleBanner)}>Change banner Picture</button>
-        </div>
-         {toggleAvatar ? <form id='profile-pic-upload'>
-                <label>Title: </label>
-                <input type='text' name='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                <br />
-                <label>{'Upload Picture: '}</label>
-                <input type="file" name='image' onChange={(e)=>setFile(e.target.files[0])} />
-                <button onClick={uploadImage}>Upload</button>
-            </form> : <></>}
+    <div className="your-account">
+      <div className="heading">
+        <h3>Your Account</h3>
+        <p>
+          See information about your account, download an archive of your data,
+          or learn about your account deactivation options
+        </p>
+      </div>
+      <div className="account-options">
+        <button
+          style={buttonStyle}
+          className="round-button option"
+          onClick={() => setToggleAvatar(!toggleAvatar)}
+        >
+          Change profile Picture
+        </button>
+        <button
+          style={buttonStyle}
+          className="round-button option"
+          onClick={() => setToggleBanner(!toggleBanner)}
+        >
+          Change banner Picture
+        </button>
+      </div>
+      {toggleAvatar ? (
+        <form id="profile-pic-upload">
+          <label>Title: </label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <br />
+          <label>{"Upload Picture: "}</label>
+          <input
+            type="file"
+            name="image"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <button onClick={uploadImage}>Upload</button>
+        </form>
+      ) : (
+        <></>
+      )}
 
-         {toggleBanner ? <form id='profile-pic-upload'>
-                <label>Title: </label>
-                <input type='text' name='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                <br />
-                <label>{'Upload Picture: '}</label>
-                <input type="file" name='image' onChange={(e)=>setFile(e.target.files[0])} />
-                <button onClick={uploadBanner}>Upload</button>
-            </form> : <></>}
+      {toggleBanner ? (
+        <form id="profile-pic-upload">
+          <label>Title: </label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <br />
+          <label>{"Upload Picture: "}</label>
+          <input
+            type="file"
+            name="image"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <button onClick={uploadBanner}>Upload</button>
+        </form>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
--- client/src/components/account/Settings.js
+++ client/src/components/account/Settings.js
@@ -1,103 +1,106 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {Route, Switch} from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 
-import './styles/style.css'
-import {storage} from '../../firebase'
-import {ref, uploadBytes, listAll, getDownloadURL} from 'firebase/storage'
-import {v4} from 'uuid'
+import "./styles/style.css";
+import { storage } from "../../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
 import SettingsSidebar from "./SettingsSidebar";
 import Account from "./Account";
 import Notifications from "./Notifications";
 // import uploadImage from "./utils/uploadUtils";
 
-function Settings({login, setLogin}) {
-    const [title, setTitle] = useState('')
-    const [avatarUrl, setAvatarUrl] = useState('')
-    const [bannerUrl, setBannerUrl] = useState('')
-    const [file, setFile] = useState(null)
+function Settings({ login, setLogin }) {
+  const [title, setTitle] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [bannerUrl, setBannerUrl] = useState("");
+  const [file, setFile] = useState(null);
 
-    
-    const uploadImage = (e) => {
-        e.preventDefault()
-        if (file==null) return;
-        const imageRef = ref(storage, `images/${file.name+v4()}`)
-        const snapshot = uploadBytes(imageRef, file)
-        const url = getDownloadURL(snapshot.ref)
-        setAvatarUrl(url)
-    }
+  const uploadBanner = async (e) => {
+    e.preventDefault();
+    if (file == null) return;
+    const imageRef = ref(storage, `images/${file.name + v4()}`);
+    const snapshot = await uploadBytes(imageRef, file);
+    const url = await getDownloadURL(snapshot.ref);
+    setBannerUrl(url);
+  };
 
-    const uploadBanner = async (e) => {
-        e.preventDefault()
-        if (file==null) return;
-        const imageRef = ref(storage, `images/${file.name+v4()}`)
-        const snapshot = await uploadBytes(imageRef, file)
-        const url = await getDownloadURL(snapshot.ref)
-        setBannerUrl(url)
+  const uploadImage = async (e) => {
+    e.preventDefault();
+    if (file == null) return;
+    const imageRef = ref(storage, `images/${file.name + v4()}`);
+    const snapshot = await uploadBytes(imageRef, file);
+    const url = await getDownloadURL(snapshot.ref);
+    setAvatarUrl(url);
+  };
 
-    }
-
-
-
-    useEffect(()=>{
-        if (avatarUrl!=='') {
-            fetch(`/users/${login.user.id}`,{
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({avatar: avatarUrl})
-            }).then((r)=>{
-                if (r.ok) {
-                    r.json().then((data)=>{
-                        setLogin({...login, user: data})
-                        setAvatarUrl('')
-                        setFile(null)
-                    })
-                } else {
-                    r.json().then((error)=>console.log(error))
-                }
-            })
+  useEffect(() => {
+    if (avatarUrl !== "") {
+      fetch(`/users/${login.user.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ avatar: avatarUrl }),
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((data) => {
+            setLogin({ ...login, user: data });
+            setAvatarUrl("");
+            setFile(null);
+          });
+        } else {
+          r.json().then((error) => console.log(error));
         }
-    },[avatarUrl, login, setLogin])
+      });
+    }
+  }, [avatarUrl, login, setLogin]);
 
-    useEffect(()=>{
-        if (bannerUrl!=='') {
-            fetch(`/users/${login.user.id}`,{
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({banner: bannerUrl})
-            }).then((r)=>{
-                if (r.ok) {
-                    r.json().then((data)=>{
-                        setLogin({...login, user: data})
-                        setBannerUrl('')
-                        setFile(null)
-                    })
-                } else {
-                    r.json().then((error)=>console.log(error))
-                }
-            })
+  useEffect(() => {
+    if (bannerUrl !== "") {
+      fetch(`/users/${login.user.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ banner: bannerUrl }),
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((data) => {
+            setLogin({ ...login, user: data });
+            setBannerUrl("");
+            setFile(null);
+          });
+        } else {
+          r.json().then((error) => console.log(error));
         }
-    },[bannerUrl, login, setLogin])
+      });
+    }
+  }, [bannerUrl, login, setLogin]);
 
-    return (
-        <div className="settings">
-            <SettingsSidebar/>
-            <div className="content">
-                <Switch>
-                    <Route path='/settings/account' >
-                        <Account title={title} setTitle={setTitle} file={file} setFile={setFile} uploadImage={uploadImage} uploadBanner={uploadBanner}/>
-                    </Route>
-                    <Route path='/settings/notifications' >
-                        <Notifications/>
-                    </Route>
-                </Switch>
-            </div>
-        </div>
-    )
+  return (
+    <div className="settings">
+      <SettingsSidebar />
+      <div className="content">
+        <Switch>
+          <Route path="/settings/account">
+            <Account
+              title={title}
+              setTitle={setTitle}
+              file={file}
+              setFile={setFile}
+              uploadImage={uploadImage}
+              uploadBanner={uploadBanner}
+            />
+          </Route>
+          <Route path="/settings/notifications">
+            <Notifications />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
